f2442325869ef3de8df2b788495cd830fd7635f8
Lukanite <lukanite@gmail.com>
2015-05-19 19:49:58 +0000

    Added loading bar for refresh

--- lights.py
+++ lights.py
@@ -23,7 +23,8 @@ offlinecount = 0
 p831count = 0
 eventcount = 0
 quit = 0
-
+timerun = 29
+dataerror = 0
 class ledControl(threading.Thread):
 	def run(self):
 		while quit == 0:
@@ -35,13 +36,15 @@ class ledControl(threading.Thread):
 			if lightvalue < 0:
 				lightvalue=0
 			anim = ColorChase(leds, color=colors.wheel_color(lightvalue))
-			anim.run(fps=5,max_steps=48)
+			anim.run(fps=30,max_steps=288)
 class ColorChase(BaseStripAnim):
 	"""Chase one pixel down the strip."""
 	global diskcount
 	global offlinecount
 	global p831count
 	global eventcount
+	global timerun
+	global dataerror
 	def __init__(self, led, color, width=1, start=0, end=-1):
 		super(ColorChase, self).__init__(led, start, end)
 		self._color = color
@@ -52,27 +55,60 @@ class ColorChase(BaseStripAnim):
 		if (count > 0):
 			for j in range(count): #8 LEDS per indicator
 				self._led.set(substart + j, colors.Red)
-			if (self._step%2): #flash
+			if ((self._step/6)%2): #flash at 5fps
 				for k in range(count, 8):
 					self._led.set(substart + k, colors.color_scale((255,100,25),255))
 			else: #all off
 				for k in range(count, 8):
 					self._led.set(substart + k, colors.color_scale((255,100,0),0))
 		else: #no problems
-					self._led.set(substart + self._step%8, colors.Green)
+					self._led.set(substart + (self._step/6)%8, colors.Green)
+	def progressbar(self): #Max 288 steps
+		numlit = (((30-timerun)*10)/30)
+		if (dataerror != 0): #flash red
+			if ((self._step/6)%2): #flash at 5fps
+				for n in range(10):
+					self._led.set(47-n,colors.Red)
+			else:
+				for n in range(10):
+					self._led.set(47-n,colors.color_scale((255,100,0),0))
+		else:
+			if (timerun < 29): 
+				self._led.set(37,colors.Green) #Start indicator
+				for m in range(numlit,10):
+					self._led.set(47-m,colors.color_scale((255,100,0),0))
+				for l in range(numlit):
+					self._led.set(47-l,colors.color_scale((255-(255*timerun/30),255-(255*timerun/30),255),255))
+			else:
+				if ((self._step/6)%2): #flash at 5fps
+					for n in range(10):
+						self._led.set(47-n,colors.White)
+				else:
+					for n in range(10):
+						self._led.set(47-n,colors.color_scale((255,100,0),0))
 	def step(self, amt = 1):
 		self._led.all_off() #because I am lazy
-		for i in range(self._width):
-			self._led.set((self._start + self._step + i + 0)%12 + 36, colors.Red)
-			self._led.set((self._start + self._step + i + 2)%12 + 36, colors.color_scale((255,100,25),255))
-			self._led.set((self._start + self._step + i + 4)%12 + 36, colors.Orange)
-			self._led.set((self._start + self._step + i + 6)%12 + 36, colors.Green)
-			self._led.set((self._start + self._step + i + 8)%12 + 36, colors.Blue)
-			self._led.set((self._start + self._step + i + 10)%12 + 36, colors.Violet)
 		self.setlights(0,diskcount)
 		self.setlights(8,offlinecount)
 		self.setlights(16,eventcount)
 		self.setlights(24,p831count)
+		self.progressbar()
+		self._step += amt
+		overflow = (self._start + self._step) - 288
+		if overflow >= 0:
+			self._step = overflow
+class ColorWipe(BaseStripAnim):
+	"""Fill the dots progressively along the strip."""
+
+	def __init__(self, led, color, start=0, end=-1):
+		super(ColorWipe, self).__init__(led, start, end)
+		self._color = color
+
+	def step(self, amt = 1):
+		if self._step == 0:
+			self._led.all_off()
+		for i in range(amt):
+			self._led.set(self._start + self._step - i, self._color)
 		self._step += amt
 		overflow = (self._start + self._step) - self._end
 		if overflow >= 0:
@@ -93,7 +129,7 @@ def authenticate():
 def getData():
 	c = pycurl.Curl()
 	fetchurl = "http://vcuhststorit.vcuhs.mcvh-vcu.edu/api/alerts.json?filter=recent&date=" + time.strftime("%Y-%m-%d") #only today
-	fetchurl = "http://vcuhststorit.vcuhs.mcvh-vcu.edu/api/alerts.json?filter=recent" #this week
+	#fetchurl = "http://vcuhststorit.vcuhs.mcvh-vcu.edu/api/alerts.json?filter=recent" #this week
 	c.setopt(pycurl.COOKIEFILE, 'cookie.txt')
 	c.setopt(pycurl.URL, fetchurl)
 	c.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
@@ -109,16 +145,21 @@ while True:
 			getData()
 			rdata = json.loads(data.getvalue())
 			data = cStringIO.StringIO()
+			dataerror = 0
 		except:
 			print "Cookie expired, refreshing"
 			print data.getvalue()
 			data = cStringIO.StringIO()
 			authenticate()
 			getData()
-		try:
-			rdata = json.loads(data.getvalue())
-		except:
-			rdata = rdata
+			try:
+				rdata = json.loads(data.getvalue())
+				dataerror = 0
+			except:
+				rdata = rdata
+				print "Error loading data!"
+				dataerror = 1
+				raise
 		count = 0
 		diskcount = 0
 		offlinecount = 0
@@ -140,7 +181,9 @@ while True:
 		print "   Offline: " + str(offlinecount)
 		print "  DMS_P831: " + str(p831count)
 		print "     Event: " + str(eventcount)
-		time.sleep(30)
+		for t in range(30):		
+			time.sleep(1)
+			timerun = t
 	except:
 		print "Quitting..."
 		quit = 1
